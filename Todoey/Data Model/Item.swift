75e61f0087090a06c03e06bd320182580351b8c1
felipe raul <feliperamirez@familiasrestauradas.org>
2018-10-30 00:19:55 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by FELIPE on 10/29/18.
+//  Copyright © 2018 FELIPE. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
